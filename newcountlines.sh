9ebbb2e360857b0e00553827ccd460cdc1f247b2
AdamMarti <martigarciaadam@gmail.com>
2024-10-31 15:00:34 +0100

    This is a simplified version of the code

--- newcountlines.sh
+++ newcountlines.sh
@@ -1,17 +1,5 @@
 for path in "$@"; do
     
-    if [ -d "$path" ]; then
-        echo "Processing directory: $path"
-        find "$path" -type f | while read -r file; do
-            line_count=$(wc -l < "$file")
-            if [ "$line_count" -eq 0 ]; then
-                echo "$file has 0 lines."
-            elif [ "$line_count" -eq 1 ]; then
-                echo "$file has 1 line."
-            else
-                echo "$file has more than 1 line."
-            fi
-        done
 
     elif [ -f "$path" ]; then
         echo "Processing file: $path"
@@ -24,8 +12,6 @@ for path in "$@"; do
             echo "$path has more than 1 line."
         fi
 
-    else
-        echo "Warning: $path is not a valid file or directory."
     fi
 done
 
