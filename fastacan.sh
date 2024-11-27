2ca8fc16c0530056ea04a3dd9698833039b04401
AdamMarti <martigarciaadam@gmail.com>
2024-11-27 20:41:02 +0100

    Updated version 2

--- fastacan.sh
+++ fastacan.sh
@@ -20,15 +20,55 @@ else
 	folderX=$2 
 fi
 
+#We will still define another variable given that this piece of code will be used many times
+
+MainFun=$(find $folderX -type f -name "*.fa" -or -name "*.fasta")
+
 #2. Tell me how many fa/fasta files there are in the directory and how many unique fasta IDs. 
 echo =============================================GENERAL REPORT=======================================================
 echo
-echo NUMBER OF FILES 
-echo "There are $(find $folderX -type f -name "*.fa" -or -name "*.fasta" | wc -l) fasta files in this directory."
+#FALTA POSAR BÉ LA CONCORDÀNCIA DE GÈNERE I NOMBRE ( 1 O MÉS DE 1)
+echo NUMBER OF FILES  
+if [[ $MainFun -eq 1 ]]
+then
+	echo "There is 1 fasta file in this directory."
+elif [[ $MainFun -eq 0 ]]
+	echo "There are none fa/fasta files in this directory."
+else
+	echo "There are $(find $folderX -type f -name "*.fa" -or -name "*.fasta") fa/fasta files in this directory."
+fi
+
 echo
+#FALTA POSAR BÉ LA CONCORDÀNCIA DE GÈNERE I NOMBRE ( 1 O MÉS DE 1)
+
+
+
 echo UNIQUE FASTA IDs
+
+
+
+
 IdCounts=$(find $folderX -type f -name "*.fa" -or -name "*.fasta" | while read i; do
 	awk '/^>.*/ {print $0}' $i #We interpret the ID as the whole line after the ">" symbol
 	done | sort | uniq | wc -l)
-echo "There are $IdCounts unique fa/fasta IDs found in the different fa/fasta files in $i". 
+echo "There are $IdCounts unique fa/fasta IDs found in the different fa/fasta files in this directory". 
+echo
+echo
+echo ================================================FILE REPORTS=======================================================
+for j in $(find $folderX -type f -name "*.fa" -or -name "*.fasta"); do
+Nseq=$(awk '/^>.*/ {print $0}' $j | wc -l)
+LSeq=$(grep -vh "^>" $j | grep -o "[a-zA-Z]" | wc -l)
+echo
+echo ==="FILENAME: $j"
+echo ==="NUMBER OF SEQUENCES: $Nseq"
+	if [[ -h $j ]]
+	then
+		echo ==="TYPE OF FILE: Symbolic link"
+	else
+		echo ==="TYPE OF FILE: Not a Symbolic link"
+	fi
+echo ==="TOTAL CHARACTERS: $LSeq"
+echo 
+echo --------------------------------------------- File separator ------------------------------------------------------
+done
 
